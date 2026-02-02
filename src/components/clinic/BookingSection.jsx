c0f57ed61253b680ba4e6514a92493301697745a
Leonel Llorens <leollorens04@gmail.com>
2026-02-02 22:33:18 +0000

    File changes

--- src/components/clinic/BookingSection.jsx
+++ src/components/clinic/BookingSection.jsx
@@ -201,7 +201,7 @@ export default function BookingSection({
 
           <div className="mt-12 flex items-center justify-center gap-3">
             {steps.map((step, idx) => (
-              <React.Fragment key={step.id}>
+              <div key={step.id} className="flex items-center gap-3">
                 <div className="flex flex-col items-center gap-2">
                   <div
                     className="flex h-10 w-10 items-center justify-center rounded-full border-2 text-sm font-medium transition-all"
@@ -225,7 +225,7 @@ export default function BookingSection({
                     }}
                   />
                 )}
-              </React.Fragment>
+              </div>
             ))}
           </div>
         </div>
